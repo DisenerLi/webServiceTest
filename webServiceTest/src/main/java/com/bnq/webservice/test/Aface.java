e484187ffca59227d6a5c3c2ff19658d87c782cc
liqiang <qiang.li@b-and-qchina.com>
2017-11-13 14:00:26 +0800

    rm webserviceTest directory

--- webServiceTest/src/main/java/com/bnq/webservice/test/Aface.java
+++
@@ -1,8 +0,0 @@
-package com.bnq.webservice.test;
-
-/**
- * Created by LJ0000276 on 2017/3/30.
- */
-public interface Aface {
-    void send();
-}
--- webServiceTest/src/main/java/com/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tmp.web/ws/hello")
-package com.client;
